976f4b308dc6ae1eb8311069b7f80f1f0db9b7d4
ajeet <ajeetx@email.com>
2023-05-04 13:37:50 +1000

    remove vendor seed

--- risk.control.system/Seeds/DatabaseSeed.cs
+++ risk.control.system/Seeds/DatabaseSeed.cs
@@ -545,76 +545,76 @@ namespace risk.control.system.Seeds
             var currentPinCode = "515631";
             var currentDistrict = "ANANTAPUR";
             var currentState = "AD";
-            try
-            {
-                var pin = context.PinCode.FirstOrDefault(s => s.Code == currentPinCode);
-            }
-            catch (Exception ex)
-            {
+            //try
+            //{
+            //    var pin = context.PinCode.FirstOrDefault(s => s.Code == currentPinCode);
+            //}
+            //catch (Exception ex)
+            //{
 
-                throw;
-            }
-            var TataAig = new ClientCompany
-            {
-                ClientCompanyId = Guid.NewGuid().ToString(),
-                Name = "TATA AIG INSURANCE",
-                Addressline = "100 GOOD STREET ",
-                Branch = "FOREST HILL CHASE",
-                City = "FOREST HILL",
-                Code = "TA001",
-                CountryId = indiaCountry.Entity.CountryId,
-                DistrictId = context.District.FirstOrDefault(s => s.Name == currentDistrict)?.DistrictId ?? default!,
-                StateId = context.State.FirstOrDefault(s => s.Code.StartsWith(currentState))?.StateId ?? default!,
-                PinCodeId = context.PinCode.FirstOrDefault(s => s.Code == currentPinCode)?.PinCodeId ?? default!,
-                Description = "CORPORATE OFFICE ",
-                Email = "tata-aig@mail.com",
-                PhoneNumber = "(03) 88004739",
-            };
+            //    throw;
+            //}
+            //var TataAig = new ClientCompany
+            //{
+            //    ClientCompanyId = Guid.NewGuid().ToString(),
+            //    Name = "TATA AIG INSURANCE",
+            //    Addressline = "100 GOOD STREET ",
+            //    Branch = "FOREST HILL CHASE",
+            //    City = "FOREST HILL",
+            //    Code = "TA001",
+            //    CountryId = indiaCountry.Entity.CountryId,
+            //    DistrictId = context.District.FirstOrDefault(s => s.Name == currentDistrict)?.DistrictId ?? default!,
+            //    StateId = context.State.FirstOrDefault(s => s.Code.StartsWith(currentState))?.StateId ?? default!,
+            //    PinCodeId = context.PinCode.FirstOrDefault(s => s.Code == currentPinCode)?.PinCodeId ?? default!,
+            //    Description = "CORPORATE OFFICE ",
+            //    Email = "tata-aig@mail.com",
+            //    PhoneNumber = "(03) 88004739",
+            //};
 
-            var tataAigCompany = await context.ClientCompany.AddAsync(TataAig);
+            //var tataAigCompany = await context.ClientCompany.AddAsync(TataAig);
 
-            //CREATE VENDOR COMPANY
+            ////CREATE VENDOR COMPANY
 
-            var listOfSericesWithPinCodes = new List<VendorInvestigationServiceType>
-            {
-                new VendorInvestigationServiceType{
-                    InvestigationServiceTypeId = claimNonComprehensiveService.Entity.InvestigationServiceTypeId,
-                    Price = 99,
-                    StateId = context.State.FirstOrDefault(s => s.Code.StartsWith(currentState))?.StateId ?? default!,
-                    LineOfBusinessId = claimCaseType.Entity.LineOfBusinessId,
-                    PincodeServices = new List<ServicedPinCode>
-                    {
-                        new ServicedPinCode
-                        {
-                            Pincode = context.PinCode.FirstOrDefault(s => s.Code == currentPinCode)?.Name ?? default !
-                        }
-                    }
-                }
-            };
+            //var listOfSericesWithPinCodes = new List<VendorInvestigationServiceType>
+            //{
+            //    new VendorInvestigationServiceType{
+            //        InvestigationServiceTypeId = claimNonComprehensiveService.Entity.InvestigationServiceTypeId,
+            //        Price = 99,
+            //        StateId = context.State.FirstOrDefault(s => s.Code.StartsWith(currentState))?.StateId ?? default!,
+            //        LineOfBusinessId = claimCaseType.Entity.LineOfBusinessId,
+            //        PincodeServices = new List<ServicedPinCode>
+            //        {
+            //            new ServicedPinCode
+            //            {
+            //                Pincode = context.PinCode.FirstOrDefault(s => s.Code == currentPinCode)?.Name ?? default !
+            //            }
+            //        }
+            //    }
+            //};
 
-            var abcVendor = new Vendor
-            {
-                Name = "abc investigation agency",
-                Addressline = "1, Main Road  ",
-                Branch = "MAHATTAN",
-                City = "FOREST HILL",
-                Code = "VA001",
-                ActivatedDate = DateTime.Now,
-                AgreementDate = DateTime.Now,
-                BankName = "WESTPAC",
-                BankAccountNumber = "1234567",
-                IFSCCode = "IFSC100",
-                CountryId = indiaCountry.Entity.CountryId,
-                DistrictId = context.District.FirstOrDefault(s => s.Name == currentDistrict)?.DistrictId ?? default!,
-                StateId = context.State.FirstOrDefault(s => s.Code.StartsWith(currentState))?.StateId ?? default!,
-                PinCodeId = context.PinCode.FirstOrDefault(s => s.Code == currentPinCode)?.PinCodeId ?? default!,
-                Description = "HEAD OFFICE ",
-                Email = "abc@vendor.com",
-                PhoneNumber = "(04) 123 234",
-                VendorInvestigationServiceTypes = listOfSericesWithPinCodes
-            };
+            //var abcVendor = new Vendor
+            //{
+            //    Name = "abc investigation agency",
+            //    Addressline = "1, Main Road  ",
+            //    Branch = "MAHATTAN",
+            //    City = "FOREST HILL",
+            //    Code = "VA001",
+            //    ActivatedDate = DateTime.Now,
+            //    AgreementDate = DateTime.Now,
+            //    BankName = "WESTPAC",
+            //    BankAccountNumber = "1234567",
+            //    IFSCCode = "IFSC100",
+            //    CountryId = indiaCountry.Entity.CountryId,
+            //    DistrictId = context.District.FirstOrDefault(s => s.Name == currentDistrict)?.DistrictId ?? default!,
+            //    StateId = context.State.FirstOrDefault(s => s.Code.StartsWith(currentState))?.StateId ?? default!,
+            //    PinCodeId = context.PinCode.FirstOrDefault(s => s.Code == currentPinCode)?.PinCodeId ?? default!,
+            //    Description = "HEAD OFFICE ",
+            //    Email = "abc@vendor.com",
+            //    PhoneNumber = "(04) 123 234",
+            //    VendorInvestigationServiceTypes = listOfSericesWithPinCodes
+            //};
 
-            var abcVendorCompany = await context.Vendor.AddAsync(abcVendor);
+            //var abcVendorCompany = await context.Vendor.AddAsync(abcVendor);
 
             #endregion
 
