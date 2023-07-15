ffae661db70ad15d0b2ec02595e4303730f59ae3
ajeet <ajeetx@email.com>
2023-07-15 15:04:10 +1000

    improvement

--- risk.control.system/wwwroot/js/site.js
+++ risk.control.system/wwwroot/js/site.js
@@ -117,31 +117,6 @@
             },
         });
     });
-    GetWeekly('Claim', 'GetWeeklyClaim', 'container-claim');
-    GetWeeklyTat('Claim', 'GetClaimWeeklyTat', 'container-claim-tat');
-    GetWeeklyPie('Claim', 'GetWeeklyClaim', 'container-claim-pie');
-
-    GetChart('Claim', 'GetClaimChart', 'container-monthly-claim')
-
-    $("#btnWeeklyReport").click(function () {
-        GetWeekly('Claim', 'GetWeeklyClaim', 'container-claim');
-    })
-
-    $("#btnMonthlyReport").click(function () {
-        GetMonthly('Claim', 'GetMonthlyClaim', 'container-claim');
-    })
-    $("#btnWeeklyPie").click(function () {
-        GetWeeklyPie('Claim', 'GetWeeklyClaim', 'container-claim-pie');
-    })
-    $("#btnMonthlyPie").click(function () {
-        GetMonthlyPie('Claim', 'GetMonthlyClaim', 'container-claim-pie');
-    })
-    $("#btnWeeklyTat").click(function () {
-        GetWeeklyTat('Claim', 'GetClaimWeeklyTat', 'container-claim-tat');
-    })
-    $("#btnMonthlyTat").click(function () {
-        //GetMonthly('Claim', 'GetClaimWeeklyTat', 'container-claim-tat');
-    })
 });
 
 function checkIfAllChecked(elements) {
