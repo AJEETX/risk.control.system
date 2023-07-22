c7c10211a75e680876c040ccc45bb8240aec1c1e
ajeet <ajeetx@email.com>
2023-07-22 15:19:54 +1000

    all end point added

--- risk.control.system/Controllers/Api/AgentController.cs
+++ risk.control.system/Controllers/Api/AgentController.cs
@@ -5,9 +5,13 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
+using NToastNotify;
+
 using risk.control.system.AppConstant;
 using risk.control.system.Data;
 using risk.control.system.Models;
+using risk.control.system.Models.ViewModel;
+using risk.control.system.Services;
 
 namespace risk.control.system.Controllers.Api
 {
@@ -16,14 +20,18 @@ namespace risk.control.system.Controllers.Api
     public class AgentController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
+        private readonly IClaimsInvestigationService claimsInvestigationService;
+        private readonly IMailboxService mailboxService;
 
-        public AgentController(ApplicationDbContext context)
+        public AgentController(ApplicationDbContext context, IClaimsInvestigationService claimsInvestigationService, IMailboxService mailboxService)
         {
             this._context = context;
+            this.claimsInvestigationService = claimsInvestigationService;
+            this.mailboxService = mailboxService;
         }
 
         [AllowAnonymous]
-        public async Task<IActionResult> Index(string currentUserEmail)
+        public async Task<IActionResult> Index(string email)
         {
             IQueryable<ClaimsInvestigation> applicationDbContext = _context.ClaimsInvestigation
                 .Include(c => c.CaseLocations)
@@ -52,7 +60,7 @@ namespace risk.control.system.Controllers.Api
             var assignedToAgentStatus = _context.InvestigationCaseSubStatus.FirstOrDefault(
                         i => i.Name.ToUpper() == CONSTANTS.CASE_STATUS.CASE_SUBSTATUS.ASSIGNED_TO_AGENT);
 
-            var vendorUser = _context.VendorApplicationUser.FirstOrDefault(c => c.Email == currentUserEmail);
+            var vendorUser = _context.VendorApplicationUser.FirstOrDefault(c => c.Email == email);
 
             if (vendorUser != null)
             {
@@ -63,7 +71,7 @@ namespace risk.control.system.Controllers.Api
                 {
                     item.CaseLocations = item.CaseLocations.Where(c => c.VendorId == vendorUser.VendorId
                         && c.InvestigationCaseSubStatusId == assignedToAgentStatus.InvestigationCaseSubStatusId
-                        && c.AssignedAgentUserEmail == currentUserEmail)?.ToList();
+                        && c.AssignedAgentUserEmail == email)?.ToList();
                     if (item.CaseLocations.Any())
                     {
                         claimsAssigned.Add(item);
@@ -72,6 +80,39 @@ namespace risk.control.system.Controllers.Api
                 return Ok(claimsAssigned);
             }
             return Unauthorized();
+        }
+
+        [AllowAnonymous]
+        [HttpGet("get")]
+        public async Task<IActionResult> Get(string email, string claimId)
+        {
+            var claimsInvestigation = _context.ClaimsInvestigation
+                .Include(c => c.LineOfBusiness)
+                .FirstOrDefault(c => c.ClaimsInvestigationId == claimId
+                && c.CurrentUserEmail == email);
+            var assignedToAgentStatus = _context.InvestigationCaseSubStatus.FirstOrDefault(
+                       i => i.Name.ToUpper() == CONSTANTS.CASE_STATUS.CASE_SUBSTATUS.ASSIGNED_TO_AGENT);
+            var claimCase = _context.CaseLocation
+                .Include(c => c.ClaimsInvestigation)
+                .Include(c => c.PinCode)
+                .Include(c => c.ClaimReport)
+                .Include(c => c.District)
+                .Include(c => c.State)
+                .FirstOrDefault(c => c.ClaimsInvestigationId == claimId
+                && c.InvestigationCaseSubStatusId == assignedToAgentStatus.InvestigationCaseSubStatusId
+                    );
+            return Ok(new ClaimsInvestigationVendorsModel { CaseLocation = claimCase, ClaimsInvestigation = claimsInvestigation });
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> Post(string email, string remarks, string claimId, long locId)
+        {
+            await claimsInvestigationService.SubmitToVendorSupervisor(email, locId, claimId, remarks);
+
+            await mailboxService.NotifyClaimReportSubmitToVendorSupervisor(email, claimId, locId);
+
+            return Ok();
         }
     }
 }
