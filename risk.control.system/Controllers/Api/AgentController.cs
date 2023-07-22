6205dc5e91921a2ef2cafa743e2b28b99018cdbd
ajeet <ajeetx@email.com>
2023-07-22 14:56:12 +1000

    mobile app

---
+++ risk.control.system/Controllers/Api/AgentController.cs
@@ -0,0 +1,77 @@
+﻿using System.Security.Claims;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using risk.control.system.AppConstant;
+using risk.control.system.Data;
+using risk.control.system.Models;
+
+namespace risk.control.system.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AgentController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AgentController(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+
+        [AllowAnonymous]
+        public async Task<IActionResult> Index(string currentUserEmail)
+        {
+            IQueryable<ClaimsInvestigation> applicationDbContext = _context.ClaimsInvestigation
+                .Include(c => c.CaseLocations)
+                .ThenInclude(c => c.ClaimReport)
+                .Include(c => c.ClientCompany)
+                .Include(c => c.CaseEnabler)
+                .Include(c => c.CaseLocations)
+                .ThenInclude(c => c.InvestigationCaseSubStatus)
+                .Include(c => c.CaseLocations)
+                .ThenInclude(c => c.PinCode)
+                .Include(c => c.CaseLocations)
+                .ThenInclude(c => c.Vendor)
+                .Include(c => c.Vendor)
+                .Include(c => c.CostCentre)
+                .Include(c => c.Country)
+                .Include(c => c.District)
+                .Include(c => c.InvestigationCaseStatus)
+                .Include(c => c.InvestigationCaseSubStatus)
+                .Include(c => c.InvestigationServiceType)
+            .Include(c => c.LineOfBusiness)
+            .Include(c => c.PinCode)
+            .Include(c => c.State);
+
+            var allocatedStatus = _context.InvestigationCaseSubStatus.FirstOrDefault(
+                        i => i.Name.ToUpper() == CONSTANTS.CASE_STATUS.CASE_SUBSTATUS.ALLOCATED_TO_VENDOR);
+            var assignedToAgentStatus = _context.InvestigationCaseSubStatus.FirstOrDefault(
+                        i => i.Name.ToUpper() == CONSTANTS.CASE_STATUS.CASE_SUBSTATUS.ASSIGNED_TO_AGENT);
+
+            var vendorUser = _context.VendorApplicationUser.FirstOrDefault(c => c.Email == currentUserEmail);
+
+            if (vendorUser != null)
+            {
+                applicationDbContext = applicationDbContext.Where(i => i.CaseLocations.Any(c => c.VendorId == vendorUser.VendorId));
+                var claimsAssigned = new List<ClaimsInvestigation>();
+
+                foreach (var item in applicationDbContext)
+                {
+                    item.CaseLocations = item.CaseLocations.Where(c => c.VendorId == vendorUser.VendorId
+                        && c.InvestigationCaseSubStatusId == assignedToAgentStatus.InvestigationCaseSubStatusId
+                        && c.AssignedAgentUserEmail == currentUserEmail)?.ToList();
+                    if (item.CaseLocations.Any())
+                    {
+                        claimsAssigned.Add(item);
+                    }
+                }
+                return Ok(claimsAssigned);
+            }
+            return Unauthorized();
+        }
+    }
+}
