6abe68b43246f5fd52e1a35ff6635d3a7fedb063
ajeet <ajeetx@email.com>
2023-08-04 19:26:39 +1000

    validate users

--- risk.control.system/Program.cs
+++ risk.control.system/Program.cs
@@ -67,7 +67,7 @@ builder.Services.AddControllersWithViews()
 //         options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                    options.UseSqlite("Data Source=reseed.db"));
+                    options.UseSqlite("Data Source=validate-user.db"));
 
 builder.Services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
 {
