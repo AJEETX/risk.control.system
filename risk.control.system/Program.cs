a99e9faa65d1c219b978edb404c8455d2f229a63
ajeet <ajeetx@email.com>
2023-08-07 12:33:27 +1000

    db refresh

--- risk.control.system/Program.cs
+++ risk.control.system/Program.cs
@@ -69,7 +69,7 @@ builder.Services.AddControllersWithViews()
 //         options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                    options.UseSqlite("Data Source=agent-load.db"));
+                    options.UseSqlite("Data Source=mobile-api.db"));
 
 builder.Services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
 {
