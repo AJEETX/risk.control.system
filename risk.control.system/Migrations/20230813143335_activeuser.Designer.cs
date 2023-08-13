ae77914ace0dad68fca3adc6c89a5df01fb223ad
ajeet <ajeetx@email.com>
2023-08-14 00:37:14 +1000

    db refresh

--- risk.control.system/Migrations/20230813143335_activeuser.Designer.cs
+++ risk.control.system/Migrations/20230813143335_activeuser.Designer.cs
@@ -11,7 +11,7 @@ using risk.control.system.Data;
 namespace risk.control.system.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20230813060936_activeuser")]
+    [Migration("20230813143335_activeuser")]
     partial class activeuser
     {
         /// <inheritdoc />
