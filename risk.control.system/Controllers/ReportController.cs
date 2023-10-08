2b916f882d042ee55e73e915beabbbc5721fbbac
ajeet <ajeetx@email.com>
2023-10-09 07:39:52 +1100

    add map

--- risk.control.system/Controllers/ReportController.cs
+++ risk.control.system/Controllers/ReportController.cs
@@ -94,12 +94,12 @@ namespace risk.control.system.Controllers
                 var longitude = location.ClaimReport.LocationLongLat.Substring(0, longLat)?.Trim();
                 var latitude = location.ClaimReport.LocationLongLat.Substring(longLat + 1)?.Trim();
                 var longLatString = longitude + "," + latitude;
-                var url = $"https://maps.googleapis.com/maps/api/staticmap?center={longLatString}&zoom=14&size=150x200&maptype=roadmap&markers=color:red%7Clabel:S%7C{longLatString}&key=AIzaSyDXQq3xhrRFxFATfPD4NcWlHLE8NPkzH2s";
+                var url = $"https://maps.googleapis.com/maps/api/staticmap?center={longLatString}&zoom=14&size=100x200&maptype=roadmap&markers=color:red%7Clabel:S%7C{longLatString}&key=AIzaSyDXQq3xhrRFxFATfPD4NcWlHLE8NPkzH2s";
                 ViewBag.LocationUrl = url;
             }
             else
             {
-                ViewBag.LocationUrl = "https://maps.googleapis.com/maps/api/staticmap?center=32.661839,-97.263680&zoom=14&size=150x200&maptype=roadmap&markers=color:red%7Clabel:S%7C32.661839,-97.263680&key=AIzaSyDXQq3xhrRFxFATfPD4NcWlHLE8NPkzH2s";
+                ViewBag.LocationUrl = "https://maps.googleapis.com/maps/api/staticmap?center=32.661839,-97.263680&zoom=14&size=100x200&maptype=roadmap&markers=color:red%7Clabel:S%7C32.661839,-97.263680&key=AIzaSyDXQq3xhrRFxFATfPD4NcWlHLE8NPkzH2s";
             }
             if (location.ClaimReport.OcrLongLat != null)
             {
@@ -107,12 +107,12 @@ namespace risk.control.system.Controllers
                 var longitude = location.ClaimReport.OcrLongLat.Substring(0, longLat)?.Trim();
                 var latitude = location.ClaimReport.OcrLongLat.Substring(longLat + 1)?.Trim();
                 var longLatString = longitude + "," + latitude;
-                var url = $"https://maps.googleapis.com/maps/api/staticmap?center={longLatString}&zoom=14&size=150x200&maptype=roadmap&markers=color:red%7Clabel:S%7C{longLatString}&key=AIzaSyDXQq3xhrRFxFATfPD4NcWlHLE8NPkzH2s";
+                var url = $"https://maps.googleapis.com/maps/api/staticmap?center={longLatString}&zoom=14&size=100x200&maptype=roadmap&markers=color:red%7Clabel:S%7C{longLatString}&key=AIzaSyDXQq3xhrRFxFATfPD4NcWlHLE8NPkzH2s";
                 ViewBag.OcrLocationUrl = url;
             }
             else
             {
-                ViewBag.OcrLocationUrl = "https://maps.googleapis.com/maps/api/staticmap?center=32.661839,-97.263680&zoom=14&size=150x200&maptype=roadmap&markers=color:red%7Clabel:S%7C32.661839,-97.263680&key=AIzaSyDXQq3xhrRFxFATfPD4NcWlHLE8NPkzH2s";
+                ViewBag.OcrLocationUrl = "https://maps.googleapis.com/maps/api/staticmap?center=32.661839,-97.263680&zoom=14&size=100x200&maptype=roadmap&markers=color:red%7Clabel:S%7C32.661839,-97.263680&key=AIzaSyDXQq3xhrRFxFATfPD4NcWlHLE8NPkzH2s";
             }
             return View(model);
         }
