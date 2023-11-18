e7dfe2b25b86e54b095179126bc6acc60f84f92a
ajeet <ajeetx@email.com>
2023-11-19 10:38:32 +1100

    refresh db

--- risk.control.system/Program.cs
+++ risk.control.system/Program.cs
@@ -85,7 +85,7 @@ builder.Services.AddControllersWithViews()
 //         options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                    options.UseSqlite("Data Source=hdfclife.db"));
+                    options.UseSqlite("Data Source=hdfc-life.db"));
 
 builder.Services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
 {
