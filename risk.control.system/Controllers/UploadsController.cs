9fc1d262033624045a1087a73dfb827eb85281c3
ajeet <ajeetx@email.com>
2023-12-07 16:44:10 +1100

    remove

--- risk.control.system/Controllers/UploadsController.cs
+++ risk.control.system/Controllers/UploadsController.cs
@@ -77,60 +77,60 @@ namespace risk.control.system.Controllers
             return viewModel;
         }
 
-        [HttpPost]
-        public async Task<IActionResult> Index(IFormFile postedFile)
-        {
-            if (postedFile != null)
-            {
-                string path = Path.Combine(webHostEnvironment.WebRootPath, "upload-case");
-                if (!Directory.Exists(path))
-                {
-                    Directory.CreateDirectory(path);
-                }
+        //[HttpPost]
+        //public async Task<IActionResult> Index(IFormFile postedFile)
+        //{
+        //    if (postedFile != null)
+        //    {
+        //        string path = Path.Combine(webHostEnvironment.WebRootPath, "upload-case");
+        //        if (!Directory.Exists(path))
+        //        {
+        //            Directory.CreateDirectory(path);
+        //        }
 
-                string fileName = Path.GetFileName(postedFile.FileName);
-                string filePath = Path.Combine(path, fileName);
-                using (FileStream stream = new FileStream(filePath, FileMode.Create))
-                {
-                    postedFile.CopyTo(stream);
-                }
+        //        string fileName = Path.GetFileName(postedFile.FileName);
+        //        string filePath = Path.Combine(path, fileName);
+        //        using (FileStream stream = new FileStream(filePath, FileMode.Create))
+        //        {
+        //            postedFile.CopyTo(stream);
+        //        }
 
-                string csvData = await System.IO.File.ReadAllTextAsync(filePath);
-                DataTable dt = new DataTable();
-                bool firstRow = true;
-                foreach (string row in csvData.Split('\n'))
-                {
-                    if (!string.IsNullOrEmpty(row))
-                    {
-                        if (!string.IsNullOrEmpty(row))
-                        {
-                            if (firstRow)
-                            {
-                                foreach (string cell in row.Split(','))
-                                {
-                                    dt.Columns.Add(cell.Trim());
-                                }
-                                firstRow = false;
-                            }
-                            else
-                            {
-                                dt.Rows.Add();
-                                int i = 0;
-                                var output = regex.Replace(row, m => m.Value.Replace(',', '@'));
-                                var rowData = output.Split(',').ToList();
-                                foreach (string cell in rowData)
-                                {
-                                    dt.Rows[dt.Rows.Count - 1][i] = cell?.Trim() ?? NO_DATA;
-                                    i++;
-                                }
-                            }
-                        }
-                    }
-                }
+        //        string csvData = await System.IO.File.ReadAllTextAsync(filePath);
+        //        DataTable dt = new DataTable();
+        //        bool firstRow = true;
+        //        foreach (string row in csvData.Split('\n'))
+        //        {
+        //            if (!string.IsNullOrEmpty(row))
+        //            {
+        //                if (!string.IsNullOrEmpty(row))
+        //                {
+        //                    if (firstRow)
+        //                    {
+        //                        foreach (string cell in row.Split(','))
+        //                        {
+        //                            dt.Columns.Add(cell.Trim());
+        //                        }
+        //                        firstRow = false;
+        //                    }
+        //                    else
+        //                    {
+        //                        dt.Rows.Add();
+        //                        int i = 0;
+        //                        var output = regex.Replace(row, m => m.Value.Replace(',', '@'));
+        //                        var rowData = output.Split(',').ToList();
+        //                        foreach (string cell in rowData)
+        //                        {
+        //                            dt.Rows[dt.Rows.Count - 1][i] = cell?.Trim() ?? NO_DATA;
+        //                            i++;
+        //                        }
+        //                    }
+        //                }
+        //            }
+        //        }
 
-                return View(dt);
-            }
-            return Problem();
-        }
+        //        return View(dt);
+        //    }
+        //    return Problem();
+        //}
     }
 }
