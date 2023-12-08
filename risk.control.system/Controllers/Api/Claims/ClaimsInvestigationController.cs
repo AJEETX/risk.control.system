928c868dde20bb9a315a2b252ed1a55581ae6e4e
ajeet <ajeetx@email.com>
2023-12-08 15:48:26 +1100

    report

--- risk.control.system/Controllers/Api/Claims/ClaimsInvestigationController.cs
+++ risk.control.system/Controllers/Api/Claims/ClaimsInvestigationController.cs
@@ -1697,7 +1697,7 @@ namespace risk.control.system.Controllers.Api.Claims
             var companyUser = _context.ClientCompanyApplicationUser.FirstOrDefault(u => u.Email == user);
 
             var claimsSubmitted = await applicationDbContext.Where(c => c.PolicyDetail.ClientCompanyId == companyUser.ClientCompanyId &&
-                c.InvestigationCaseSubStatus.Name == CONSTANTS.CASE_STATUS.CASE_SUBSTATUS.SUBMITTED_TO_ASSESSOR &&
+                c.InvestigationCaseSubStatus.Name == CONSTANTS.CASE_STATUS.CASE_SUBSTATUS.SUBMITTED_TO_ASSESSOR ||
                 c.InvestigationCaseSubStatus.Name == CONSTANTS.CASE_STATUS.CASE_SUBSTATUS.SUBMITTED_TO_SUPERVISOR
                 )
                  .ToListAsync();
@@ -1777,7 +1777,7 @@ namespace risk.control.system.Controllers.Api.Claims
             var companyUser = _context.ClientCompanyApplicationUser.FirstOrDefault(u => u.Email == user);
 
             var claimsSubmitted = await applicationDbContext.Where(c => c.PolicyDetail.ClientCompanyId == companyUser.ClientCompanyId &&
-                c.InvestigationCaseSubStatus.Name == CONSTANTS.CASE_STATUS.CASE_SUBSTATUS.SUBMITTED_TO_ASSESSOR &&
+                c.InvestigationCaseSubStatus.Name == CONSTANTS.CASE_STATUS.CASE_SUBSTATUS.SUBMITTED_TO_ASSESSOR ||
                 c.InvestigationCaseSubStatus.Name == CONSTANTS.CASE_STATUS.CASE_SUBSTATUS.SUBMITTED_TO_SUPERVISOR
                 )
                  .ToListAsync();
