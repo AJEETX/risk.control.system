2ceee13cdd5b1c8f311c6a6fbf60e4d744be0f56
ajeet <ajeetx@email.com>
2023-12-17 14:53:49 +1100

    more

---
+++ risk.control.system/wwwroot/js/all-vendors.js
@@ -0,0 +1,50 @@
+﻿$(document).ready(function () {
+    $("#customerTable").DataTable({
+        ajax: {
+            url: '/api/Agency/AllAgencies',
+            dataSrc: ''
+        },
+        fixedHeader: true,
+        processing: true,
+        paging: true,
+        language: {
+            loadingRecords: '&nbsp;',
+            processing: '<i class="fas fa-sync fa-spin fa-4x fa-fw"></i><span class="sr-only">Loading...</span>'
+        },
+        columns: [
+            /* Name of the keys from
+            data file source */
+            {
+                "data": "id", "name": "Id", "bVisible": false
+            },
+            {
+                "sDefaultContent": "",
+                "bSortable": false,
+                "mRender": function (data, type, row) {
+                    var img = '<img src="' + row.document + '" src height="30" width="30" />';
+                    return img;
+                }
+            },
+            { "data": "domain" },
+            { "data": "name" },
+            { "data": "code" },
+            { "data": "phone" },
+            { "data": "address" },
+            { "data": "district" },
+            { "data": "state" },
+            { "data": "country" },
+            {
+                "sDefaultContent": "",
+                "bSortable": false,
+                "mRender": function (data, type, row) {
+                    var buttons = "";
+                    buttons += '<a href="/Vendors/Details?Id=' + row.id + '" class="btn btn-xs btn-info"><i class="fa fa-search"></i> Details</a>&nbsp;'
+                    buttons += '<a href="/Vendors/Edit?Id=' + row.id + '"  class="btn btn-xs btn-warning"><i class="fas fa-pen"></i> Edit</a>&nbsp;'
+                    //buttons += '<a href="Vendors/Delete?Id=' + row.id + '"  class="btn btn-xs btn-danger"><i class="fas fa-trash"></i></i> Delete</a>'
+                    return buttons;
+                }
+            }
+        ],
+        error: function (xhr, status, error) { alert('err ' + error) }
+    });
+});
