b608aeb5e9fba88d7fcd5df90ceb885f8f4d33c5
ajeet <ajeetx@email.com>
2023-12-18 19:51:59 +1100

    unsafe eval

--- risk.control.system/Program.cs
+++ risk.control.system/Program.cs
@@ -220,7 +220,7 @@ app.Use(async (context, next) =>
     context.Response.Headers.Add("Content-Security-Policy",
         "default-src 'self';" +
         "connect-src  wss: 'self' https://maps.googleapis.com; " +
-        "script-src 'unsafe-inline' 'self' https://maps.googleapis.com https://polyfill.io https://highcharts.com https://export.highcharts.com https://cdnjs.cloudflare.com; " +
+        "script-src 'unsafe-inline' 'unsafe-eval' 'self' https://maps.googleapis.com https://polyfill.io https://highcharts.com https://export.highcharts.com https://cdnjs.cloudflare.com; " +
         "style-src  'unsafe-inline' 'self' https://cdnjs.cloudflare.com/ https://fonts.googleapis.com ; " +
         "font-src 'unsafe-inline' 'self'  https://fonts.gstatic.com https://cdnjs.cloudflare.com https://fonts.googleapis.com; " +
         "img-src 'unsafe-inline' 'self'  data: blob: https://maps.gstatic.com https://maps.googleapis.com  https://developers.google.com https://hostedscan.com https://highcharts.com https://export.highcharts.com; " +
