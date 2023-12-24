2d5e6590254d15bd0ad7a19cfece695812cef1df
ajeet <ajeetx@email.com>
2023-12-24 22:51:12 +1100

    refresh Db

--- risk.control.system/Program.cs
+++ risk.control.system/Program.cs
@@ -102,7 +102,7 @@ if (prod)
 else
 {
     builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                        options.UseSqlite("Data Source=icheckify_1_0_1_0.db"));
+                        options.UseSqlite("Data Source=icheckify_1_0_1_1.db"));
 }
 
 builder.Services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
