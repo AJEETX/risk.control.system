7f01603788e8ab1119cf5a3e3581bd3a51c9dc35
ajeet <ajeetx@email.com>
2024-01-01 14:07:59 +1100

    invoice

--- risk.control.system/Controllers/ClaimsInvestigationController.cs
+++ risk.control.system/Controllers/ClaimsInvestigationController.cs
@@ -199,6 +199,31 @@ namespace risk.control.system.Controllers
             return View(invoice);
         }
 
+        [Breadcrumb(title: "Print", FromAction = "ShowInvoice")]
+        public async Task<IActionResult> PrintInvoice(string id)
+        {
+            var invoice = await _context.VendorInvoice
+                .Where(x => x.VendorInvoiceId.Equals(id))
+                .Include(x => x.ClientCompany)
+                .ThenInclude(c => c.District)
+                .Include(c => c.ClientCompany)
+                .ThenInclude(c => c.State)
+                .Include(c => c.ClientCompany)
+                .ThenInclude(c => c.Country)
+                .Include(c => c.ClientCompany)
+                .ThenInclude(c => c.PinCode)
+                .Include(x => x.Vendor)
+                .ThenInclude(v => v.State)
+                .Include(v => v.Vendor)
+                .ThenInclude(v => v.District)
+                .Include(v => v.Vendor)
+                .ThenInclude(v => v.Country)
+                .Include(i => i.InvestigationServiceType)
+                .FirstOrDefaultAsync();
+
+            return View(invoice);
+        }
+
         [Breadcrumb("Details", FromAction = "CreatePolicy", FromController = typeof(InsurancePolicyController))]
         public async Task<IActionResult> Details(string id)
         {
