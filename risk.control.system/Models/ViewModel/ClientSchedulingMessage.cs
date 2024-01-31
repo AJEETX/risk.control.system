5d8f4d418e2404fb2c5b2730d24d77ca1b470a0e
ajeet <ajeetx@email.com>
2024-02-01 10:59:06 +1100

    ignore baseurl

--- risk.control.system/Models/ViewModel/ClientSchedulingMessage.cs
+++ risk.control.system/Models/ViewModel/ClientSchedulingMessage.cs
@@ -1,10 +1,14 @@
-﻿namespace risk.control.system.Models.ViewModel
+﻿using System.Text.Json.Serialization;
+
+namespace risk.control.system.Models.ViewModel
 {
     public class ClientSchedulingMessage
     {
         public string ClaimId { get; set; }
         public string LongLat { get; set; }
         public string Time { get; set; }
+
+        [JsonIgnore]
         public string? BaseUrl { get; set; }
     }
 }
