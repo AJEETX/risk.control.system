f59a8d57743c81f432cdda2ba246bab105795e07
ajeet <ajeetx@email.com>
2024-04-08 10:12:22 +1000

    more clean

--- risk.control.system/wwwroot/js/compose-mail.js
+++ risk.control.system/wwwroot/js/compose-mail.js
@@ -109,4 +109,12 @@ $(document).ready(function () {
         }
     })
 
+
+    $('#RawMessage').summernote({
+        height: 300,                 // set editor height
+        minHeight: null,             // set minimum height of editor
+        maxHeight: null,             // set maximum height of editor
+        focus: false                  // set focus to editable area after initializing summernote
+    });
+
 });
