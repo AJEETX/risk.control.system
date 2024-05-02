d4931dd14bbeb68c07de27f68e13a90976fd0ba7
ajeet <ajeetx@email.com>
2024-05-03 08:03:32 +1000

    try this

--- risk.control.system/Helpers/GoogleApi.cs
+++ risk.control.system/Helpers/GoogleApi.cs
@@ -1,4 +1,5 @@
-﻿using Google.Cloud.Vision.V1;
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Cloud.Vision.V1;
 
 using System;
 using System.IO;
@@ -20,10 +21,18 @@ namespace risk.control.system.Helpers
         }
         public async Task<IReadOnlyList<EntityAnnotation>> DetectTextAsync(byte[] byteImage)
         {
-            var cred = Path.Combine(webHostEnvironment.WebRootPath, "icheckify-111c8d2c04f0.json");
-            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", cred);
 
-            var client = ImageAnnotatorClient.Create();
+
+            string credentialJson = Environment.GetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS_JSON");
+
+            // Create Google Credential from JSON string
+            GoogleCredential googleCredential = GoogleCredential.FromJson(credentialJson);
+
+            var client = new ImageAnnotatorClientBuilder
+            {
+                Credential = googleCredential
+            }.Build();
+
             var image = Image.FromBytes(byteImage);
             try
             {
