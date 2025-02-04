5b79149ccc5a122c980ae5509df8c5ca2f71ce0b
ajeet <ajeetx@email.com>
2025-02-04 20:02:54 +1100

    add flag on top

---
+++ risk.control.system/Models/ViewModel/TopNavigation.cs
@@ -0,0 +1,9 @@
+﻿namespace risk.control.system.Models.ViewModel
+{
+    public class TopNavigation
+    {
+        public bool CanChangePassword { get; set; }
+        public string Country { get; set; }
+        public string CountryCode { get; set; }
+    }
+}
