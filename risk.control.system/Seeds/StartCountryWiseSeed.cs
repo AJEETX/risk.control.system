34361f58b3a19de4587361447903fb440e751904
ajeet <ajeetx@email.com>
2025-09-10 17:21:32 +0530

    add envs for APP_URL, AU, Ind mobile

--- risk.control.system/Seeds/StartCountryWiseSeed.cs
+++ risk.control.system/Seeds/StartCountryWiseSeed.cs
@@ -23,16 +23,7 @@ namespace risk.control.system.Seeds
             await PinCodeStateSeed.Currencies(context);
             var countries = await PinCodeStateSeed.Countries(context);
             randomPinCode = await IndiaSeed.Seed(context, webHostEnvironment, userManager, vendorUserManager, clientUserManager, roleManager, customApiCLient, httpAccessor, countries, servicesTypes);
-            //randomPinCode = await AustraliaSeed.Seed(context, webHostEnvironment, userManager, vendorUserManager, clientUserManager, roleManager, customApiCLient, httpAccessor, countries, servicesTypes);
-
-            // seed USA
-            //var us = countries.FirstOrDefault(c => c.Code.ToLower() == "us");
-            //var usPincodes = await PinCodeStateSeed.CsvRead_Us();
-            //var usStates = usPincodes.Where(s => s.StateCode.ToLower() == "nc" ||
-            //    s.StateCode.ToLower() == "ny"
-            //    ).Select(g => g.StateCode).Distinct()?.ToList();
-            //var filteredUsPincodes = usPincodes.Where(g => usStates.Contains(g.StateCode))?.ToList();
-            //await PinCodeStateSeed.SeedPincode(context, filteredUsPincodes, us);
+            randomPinCode = await AustraliaSeed.Seed(context, webHostEnvironment, userManager, vendorUserManager, clientUserManager, roleManager, customApiCLient, httpAccessor, countries, servicesTypes);
 
             await PortalAdminSeed.Seed(context, webHostEnvironment, userManager, roleManager, randomPinCode);
         }
