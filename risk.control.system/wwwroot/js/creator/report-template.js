e524791ea1d5db91529bbe456e0f92b9931aaa32
ajeet <ajeetx@email.com>
2025-10-12 15:46:35 +1100

    edit report template

--- risk.control.system/wwwroot/js/creator/report-template.js
+++ risk.control.system/wwwroot/js/creator/report-template.js
@@ -1,15 +1,17 @@
 ﻿$(function () {
-    $("#loadTemplate").on("click", function (e) {
-        e.preventDefault();
+    $('#loadTemplate').on('click', function () {
+        const container = $("#reportTemplateContainer");
+        const caseId = $("#caseIdHidden").val();
 
-        let caseId = $("#caseIdHidden").val();
-        let container = $("#reportTemplateContainer");
+        // Load only if not already loaded
+        if (container.data("loaded")) return;
 
-        container.html("<div class='text-center p-3'><i class='fas fa-sync fa-spin'></i>...</div>");
+        container.html("<div class='text-center p-3'><i class='fas fa-sync fa-spin fa-2x'></i></div>");
 
         $.get("/CaseActive/GetReportTemplate", { caseId: caseId })
             .done(function (html) {
                 container.html(html);
+                container.data("loaded", true);
             })
             .fail(function () {
                 container.html("<div class='alert alert-danger'>Failed to load report template.</div>");
