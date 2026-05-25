0c10c679d8ed2e832fa9c56c1b5ab901af38de02
ajeet <ajeetx@email.com>
2026-05-25 10:22:10 +0530

    add invoicing to portal admin

---
+++ risk.control.system/Models/ViewModel/InvoiceDetail.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+namespace risk.control.system.Models.ViewModel
+{
+    public class InvoiceDetail
+    {
+        public VendorInvoice VendorInvoice { get; set; } = default!;
+        public string ContractNumber { get; set; } = default!;
+    }
+
+    public class VendorInvoiceIndexViewModel
+    {
+        // Holds the selected Vendor ID from the dropdown
+        public long? SelectedVendorId { get; set; }
+
+        // Strongly typed item collection for the HTML Select element
+        public List<SelectListItem> VendorList { get; set; } = new();
+    }
+}
